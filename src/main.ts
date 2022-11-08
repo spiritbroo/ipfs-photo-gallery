9c0450de5a820ac2e30bea69666e2867bcbda818
spiritbroo <rinoakbr@gmail.com>
2022-11-09 06:10:41 +0700

    bismillah menang

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,10 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import './registerServiceWorker';
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+      component: () => import('@/views/LoginPage.vue')
   },
   {
     path: '/tabs/',
@@ -13,19 +13,11 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        redirect: '/tabs/tab2'
       },
       {
         path: 'tab2',
         component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
       }
     ]
   }
